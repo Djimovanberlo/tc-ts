e0ad678b32670ef15b236cabb1121b31f0d9adc8
Djimo van Berlo <djimovanberlo@gmail.com>
2026-06-23 23:13:02 +0200

    paladin & priest

--- src/data/talents/priest/holy/tier6.ts
+++ src/data/talents/priest/holy/tier6.ts
@@ -6,9 +6,20 @@ const tier6 = [
   new Talent(
     "Spiritual Healing",
     "spell_nature_moonglow",
-    ["Increases the amount healed by your healing spells by ", "%."],
+    [
+      "Increases the amount healed by your healing spells and increases the damage of your Holy spells by ",
+      "%.",
+    ],
     5,
-    [[2, 4, 6, 8, 10]]
+    [[2, 4, 6, 8, 10]],
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    { isNew: false, isChanged: true, isMoved: false },
   ),
   null,
 ];
