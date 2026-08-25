75f18750403d205e3f80d932138cf879f579f74e
Djimo van Berlo <djimovanberlo@gmail.com>
2026-08-25 09:56:10 +0200

    more minor

--- src/data/patch-notes/race-changes/race-changes/troll.tsx
+++ src/data/patch-notes/race-changes/race-changes/troll.tsx
@@ -1,7 +1,7 @@
 export default {
   title: "Troll",
   content: [
-    "New ability: Da Voodoo Shuffle: Reduces the duration of all movement impairing effects by 15%.  Trolls be flippin' out mon!",
-    "Troll Regeneration now restores 1% health every 5 seconds.",
+    "New ability: Da Voodoo Shuffle: Reduces the duration of all movement impairing effects by 15%. Trolls be flippin' out mon!",
+    "Troll Regeneration now restores 1% health every 10 seconds.",
   ],
 };
