4b8cb26faf8a0f6f27fe202ff37cc649fc449e69
Djimo van Berlo <djimovanberlo@gmail.com>
2026-08-26 20:09:58 +0200

    typo

--- src/data/patch-notes/class-changes/classes/druid.ts
+++ src/data/patch-notes/class-changes/classes/druid.ts
@@ -2,7 +2,7 @@ export default {
   title: "Druid",
   content: [
     "Tiger’s Fury replaced with Feral Fury: now increases the damage of your next ability while in cat or bear form by 25% and generates 10 rage or 30 energy. 30 sec cooldown.",
-    "Faerie Fire (Feral) is now a base ability.",
+    "Faerie Fire (Feral) is now a baseline ability.",
     "Hurricane is now a 3 sec cast instead of channeled.",
     "Swipe now scales with attack power.",
     "Frenzied Regeneration now heals based on the druid's total health.",
